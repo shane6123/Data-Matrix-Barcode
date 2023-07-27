2ff51f01b05a847af5f45077174f7f645412132e
shane6123 <90815651+shane6123@users.noreply.github.com>
2023-07-27 15:42:19 +0530

    task done

--- App.js
+++ App.js
@@ -1,46 +1,80 @@
-import React, { useState, useEffect } from 'react';
-import { Text, View, StyleSheet, Button } from 'react-native';
 import { BarCodeScanner } from 'expo-barcode-scanner';
+import { Camera, CameraType } from 'expo-camera';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { MaterialIcons } from '@expo/vector-icons';
 
 export default function App() {
-  const [hasPermission, setHasPermission] = useState(null);
   const [scanned, setScanned] = useState(false);
-
-  useEffect(() => {
-    const getBarCodeScannerPermissions = async () => {
-      const { status } = await BarCodeScanner.requestPermissionsAsync();
-      if (status === 'granted') {
-        alert('You can use the Bar Code Scanner');
-      }else{
-        alert('You cannot use the Bar Code Scanner , Please enable the permission');
-        await BarCodeScanner.requestPermissionsAsync();
-
-      }
-      setHasPermission(status === 'granted');
-      
-    };
-    getBarCodeScannerPermissions();
-  }, []);
-
+  const [zoom, setZoom] = useState(0.1);
+  const [flashMode, setFlashMode] = useState(Camera.Constants.FlashMode.off);
+  const [type, setType] = useState(CameraType.back);
+  const [permission, requestPermission] = Camera.useCameraPermissions();
   const handleBarCodeScanned = ({ type, data }) => {
+    // Handle scanned QR code data here
     setScanned(true);
     alert(`Bar code with type ${type} and data ${data} has been scanned!`);
   };
-
-  if (hasPermission === null) {
-    return <Text>Requesting for camera permission</Text>;
+  if (!permission) {
+    // Camera permissions are still loading
+    return <View />;
   }
-  if (hasPermission === false) {
-    return <Text>No access to camera</Text>;
+
+  function handleZoomIn() {
+   if (zoom < 1 ) {
+    setZoom(zoom + 0.1   );
+   } 
+  }
+
+  function handleZoomOut() {
+    if ( zoom > 0.2)  {setZoom(zoom - 0.1);}
+  }
+  function handleToggleFlash() {
+    setFlashMode(current => (current === Camera.Constants.FlashMode.off ? Camera.Constants.FlashMode.on : Camera.Constants.FlashMode.off));
+  }
+  if (!permission.granted) {
+    // Camera permissions are not granted yet
+    return (
+      <View style={styles.container}>
+        <Text style={{ textAlign: 'center' }}>We need your permission to show the camera</Text>
+        <Button onPress={requestPermission} title="grant permission" />
+      </View>
+    );
+  }
+
+  function toggleCameraType() {
+    setType(current => (current === CameraType.back ? CameraType.front : CameraType.back));
   }
 
   return (
     <View style={styles.container}>
-      <BarCodeScanner
+       <Camera
+        style={{ flex: 1 , marginVertical: 200 ,  marginHorizontal: 50 }}
+        type={Camera.Constants.Type.back}
+        barCodeScannerSettings={{
+          barCodeTypes: [BarCodeScanner.Constants.BarCodeType.datamatrix],
+        }}
         onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
-        style={[StyleSheet.absoluteFillObject, {flex: 1}]}
+        zoom={zoom}
+        flashMode={flashMode}
+        
       />
+
       {scanned && <Button title={'Tap to Scan Again'} onPress={() => setScanned(false)} />}
+      <View style={styles.controlsContainer}>
+        <TouchableOpacity style={styles.zoomButton} onPress={handleZoomIn}>
+         <MaterialIcons name="add" size={40} color="black" />
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.zoomButton} onPress={handleZoomOut}>
+          <MaterialIcons name="remove" size={40} color="black" />
+        </TouchableOpacity>
+      </View>
+      <TouchableOpacity style={styles.controlButton} onPress={handleToggleFlash}>
+          <Text style={styles.controlText}>
+            {flashMode === Camera.Constants.FlashMode.off ? 'Flash On' : 'Flash Off'}
+          </Text>
+        </TouchableOpacity>
+
     </View>
   );
 }
@@ -48,7 +82,29 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: 'column',
     justifyContent: 'center',
+  },
+  camera: {
+    flex: 1,
+    width: '60%',
+    alignSelf: 'center',
+    height: 400,
+    marginVertical: 300,
+  },
+  buttonContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    backgroundColor: 'transparent',
+    margin: 64,
+  },
+  button: {
+    flex: 1,
+    alignSelf: 'flex-end',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: 'white',
   },
 });
